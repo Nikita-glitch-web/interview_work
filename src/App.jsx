f5f5b6fdbae6c5a8a2386f926c5078f852fc7bef
Nick <nikitalytvynov0506@gmail.com>
2024-07-15 15:15:37 +0300

    header-done

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import { Header } from './components/Header/Header';
 
 function App() {
   return (
-    <div className="content_wrapper">
       <Header />
-    </div>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,19 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react"
+import React from "react";
 import style from "./Header.module.css";
 import img from "./header_logo.png";
 import { Users } from "../Buttons/Users";
 import { SignUp } from "../Buttons/SignUp";
 
 export const Header = () => {
-    return (
-      <header className={style.header_wrapper}>
+  return (
+    <header className={style.header_wrapper}>
+      <div className={style.header_content}>
         <div className={style.header_logo_box}>
-          <img src={img} />
+          <div className={style.header_logo_wrapper}>
+            <img src={img} />
+          </div>
+          <h1 className={style.header_logo_text}>TESTTASK</h1>
         </div>
         <div className={style.header_nav_wrapper}>
           <div className={style.header_btn_wrapper}>
@@ -19,6 +23,7 @@ export const Header = () => {
             <SignUp />
           </div>
         </div>
-      </header>
-    );
-}
+      </div>
+    </header>
+  );
+};
